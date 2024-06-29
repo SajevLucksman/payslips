56e9d70ab36f0aaed4dd19fcd12a5e699ffc118a
sajev Lucksman <lsajev.it@gmail.com>
2024-06-29 09:05:46 +0530

    Payslips application - Integrated Capacitor file system and modified iOS plist files

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.dee.payslips.system',
   appName: 'Payslips',
-  webDir: 'build'
+  webDir: 'build',
+  ios: {
+    scheme: 'Payslips'
+  }
 };
 
 export default config;
