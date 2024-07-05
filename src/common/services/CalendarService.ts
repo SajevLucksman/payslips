07d22411354693accb06ad455d715e72b379c4c1
sajev Lucksman <lsajev.it@gmail.com>
2024-07-05 12:39:13 +0530

    Payslips application - Calendar service code enhancement

--- src/common/services/CalendarService.ts
+++ src/common/services/CalendarService.ts
@@ -1,14 +1,14 @@
 export default class CalendarService {
-  static getMonthName(dateString: string): string {
-    const monthNames = [
-      'January', 'February', 'March', 'April', 'May', 'June',
-      'July', 'August', 'September', 'October', 'November', 'December'
-    ];
+  private static monthNames = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
 
+  static getMonthName(dateString: string): string {
     // Extract the month number from the date string
     const monthNumber = parseInt(dateString.split('-')[1], 10);
 
     // Return the month name
-    return monthNames[monthNumber - 1];
+    return CalendarService.monthNames[monthNumber - 1];
   }
 }
